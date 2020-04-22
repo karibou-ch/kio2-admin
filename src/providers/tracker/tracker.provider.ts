dce5475df1d2dd157b9c79d42aa85ab338c6b1c1
olivier <olivier@evaletolab.ch>
2020-04-22 18:32:22 +0200

    chg: remove dead code

--- src/providers/tracker/tracker.provider.ts
+++ src/providers/tracker/tracker.provider.ts
@@ -1,13 +1,10 @@
 import { Injectable, NgZone } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
 import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 import { Events } from 'ionic-angular';
 
 /*
   Generated class for the TrackerProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular DI.
 */
 @Injectable()
@@ -24,7 +21,6 @@ export class TrackerProvider {
   };
 
   constructor(
-    public backgroundGeolocation: BackgroundGeolocation,
     public events: Events,
     public geolocation: Geolocation,
     public http: HttpClient,
@@ -32,80 +28,27 @@ export class TrackerProvider {
   ) {
   }
 
-  fetch(){
-    this.geolocation.getCurrentPosition(this.options).then(position=>{
-      this.zone.run(() => this.events.publish('location',position.coords));      
-    })    
+  fetch() {
+    this.geolocation.getCurrentPosition(this.options).then(position => {
+      this.zone.run(() => this.events.publish('location', position.coords));
+    });
   }
-  start(){
-    // Background Tracking
-    // see configuration values at : https://github.com/mauron85/cordova-plugin-background-geolocation
 
-    // let config = {
-    //   // Desired accuracy in meters. Possible values [0, 10, 100, 1000]. 
-    //   // The lower the number, the more power devoted to GeoLocation resulting in higher accuracy readings. 
-    //   // 1000 results in lowest power drain and least accurate readings.
-    //   desiredAccuracy: 100, 
-    //   // Stationary radius in meters. When stopped, the minimum distance the device must move 
-    //   // beyond the stationary location for aggressive background-tracking to engage.
-    //   stationaryRadius: 20,
-    //   // The minimum distance (measured in meters) a device must move horizontally before an update event is generated.
-    //   distanceFilter: 10,
-    //   debug: false,
-    //   interval: 10000
-    // };
+  start() {
 
     this.watch = this.geolocation.watchPosition(this.options).filter((p: any) => p.code === undefined).subscribe((position: Geoposition) => {
-      this.zone.run(() => this.events.publish('location',position.coords));
+      this.zone.run(() => this.events.publish('location', position.coords));
     });
-    
-    this.geolocation.getCurrentPosition(this.options).then(position=>{
-      this.zone.run(() => this.events.publish('location',position.coords));      
-    })
-    
-    // this.geoLocation$=Observable.combineLatest(
-    //     this.backgroundGeolocation.configure(config).map(p=>p),
-    //     this.geolocation.watchPosition(options).filter((p) => p.coords !== undefined).map(p=>p)
-    //   )
-    
-    //this.currentPosition=this.geolocation.getCurrentPosition(options);
-    
-    
-    //
-    // combine observable 
-    // this.geoLocation$=this.backgroundGeolocation.configure(config);
-    //   // simple transformation
-    //   .map(p=>p)
-    //   //
-    //   // transform observable to ConnectableObservable (multicasting)
-    //   .publishReplay(1)
-    //   //
-    //   // used to auto-connect to the source when there is >= 1 subscribers
-    //   .refCount();
 
-    // this.backgroundGeolocation.configure(config).subscribe(location =>{
-    // // Run update inside of Angular's zone
-    
-    //   this.zone.run(() => this.events.publish('location',location));
-    // }, (err) => {
-    //   //
-    //   // inform errors
-    //   this.zone.run(() => this.events.publish('location',{error:err}))  ;
-    // });
-
-
-
-    //this.backgroundGeolocation.start();
+    this.geolocation.getCurrentPosition(this.options).then(position => {
+      this.zone.run(() => this.events.publish('location', position.coords));
+    });
   }
 
   stop() {
-    //this.backgroundGeolocation.finish();
-    if(this.watch){
+    if (this.watch) {
       this.watch.unsubscribe();
-      this.watch=null;
+      this.watch = null;
     }
   }
-
-
-
 }
