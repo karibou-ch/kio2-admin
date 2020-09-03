1c0b02b6f20c58a5d7e0a0677e4c13785310ffeb
olivier <olivier@evaletolab.ch>
2020-09-03 10:25:30 +0200

    new: edit customer

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CustomersPage } from './customers.page';
+import { CustomerPage } from './customer.page';
 
 const routes: Routes = [
   {
     path: '',
     component: CustomersPage
+  },
+  {
+    path: ':id',
+    component: CustomerPage
   }
 ];
 
